27cee7cf72ed3341e4a3c89148959c358e42ea81
anime-sad <animezorolover1010@gmail.com>
2025-03-09 22:13:19 +0530

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,5 +1,9 @@
 function sendmail(){
   let parms ={
-    
+    name:document.getElementById("name").value,
+    email:document.getElementById("email").value,
+    subject:document.getElementById("subject").value,
   }
+
+  emailjs.send("service_1167kdq,template_fbvr435,parms").then(alert("Email sent!!"))
 }
